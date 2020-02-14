128b740b55d87f9baa06f3604762caacfcd830f2
Will <willdarnell@gmail.com>
2020-02-14 14:56:18 -0600

    move prior positions into its own file

---
+++ lib/components/PriorPositions.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class PriorPositions extends StatelessWidget {
+  final String title;
+  final String company;
+  final String date;
+  final String location;
+  final String description;
+
+  PriorPositions({
+    this.title,
+    this.company,
+    this.date,
+    this.location,
+    this.description
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      runSpacing: 4.0,
+      children: <Widget>[ Column(
+      
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Row(children: <Widget>[
+          Text('$title', style: TextStyle(fontFamily: 'Montserrat',))
+        ],),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+          Text('$company', textAlign: TextAlign.left, style: TextStyle(fontFamily: 'Roboto',) ), Text('$date', textAlign: TextAlign.center,), Text('$location', textAlign: TextAlign.right,)
+        ]),
+        Row(
+          children: <Widget>[
+            Flexible( child: Text('$description', style: TextStyle(fontFamily: 'Roboto',))
+            )]
+        ),
+      ]
+    )]);
+  }}
--- lib/screens/resume_screen.dart
+++ lib/screens/resume_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
-
+import 'package:call_me/components/PriorPositions.dart';
 class ResumeScreen extends StatelessWidget {
   final list = ["one", "two", "three"];
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child:  CustomScrollView(
+    return SafeArea(
+      minimum: EdgeInsets.all(15),
+      child:  CustomScrollView(
+      
   slivers: <Widget>[
     
     SliverFixedExtentList(
@@ -24,44 +27,3 @@ class ResumeScreen extends StatelessWidget {
   }
 }
 
-class PriorPositions extends StatelessWidget {
-  final String title;
-  final String company;
-  final String date;
-  final String location;
-  final String description;
-
-  PriorPositions({
-    this.title,
-    this.company,
-    this.date,
-    this.location,
-    this.description
-  });
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Wrap(
-      runSpacing: 4.0,
-      children: <Widget>[ Column(
-      
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget>[
-        Row(children: <Widget>[
-          Text('$title', style: TextStyle(fontFamily: 'Montserrat',))
-        ],),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-          Text('$company', textAlign: TextAlign.left, style: TextStyle(fontFamily: 'Roboto',) ), Text('$date', textAlign: TextAlign.center,), Text('$location', textAlign: TextAlign.right,)
-        ]),
-        Row(
-          children: <Widget>[
-            Flexible( child: Text('$description', style: TextStyle(fontFamily: 'Roboto',))
-            )]
-        ),
-      ]
-    )]);
-  }}
